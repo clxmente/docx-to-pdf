d95fdf172eec93a532b726bf2c0cafa9f6e3eeab
Clemente Solorio <37494038+clxmente@users.noreply.github.com>
2019-05-05 14:57:17 -0700

    am dumb

--- conversion.py
+++ conversion.py
@@ -10,14 +10,10 @@ for i in os.listdir(current_dir):
     if i.endswith('.docx'):
         word = comtypes.client.CreateObject('Word.Application')
         output_file_name = i[:-5] # removes .docx extension and selects just the file name for saving
-        if output_file_name.startswith("/"):
-            final_out = output_file_name[1:]  # removes the "/" at the beginning of the file name that i think is needed for the program to work
-        else:
-            final_out = output_file_name
             
-        out_file = os.path.abspath('{}'.format(final_out))
+        out_file = os.path.abspath('{}'.format(output_file_name))
 
-        doc = word.Documents.Open(current_dir  + i)
+        doc = word.Documents.Open(current_dir  + "/{}".format(i))
         doc.SaveAs(out_file, FileFormat=wdFormatPDF)
         doc.Close()
         word.Quit()
